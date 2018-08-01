2fe2844750ed2912394e580df6241184d61a03eb
mdgoogle <gengtiwin@hotmail.com>
2018-08-01 10:31:17 +0800

    a

--- Chapter04/Chapter04/Program.cs
+++ Chapter04/Chapter04/Program.cs
@@ -10,6 +10,23 @@ namespace Chapter04
     {
         static void Main(string[] args)
         {
+
+            using (northwndEntities context=new northwndEntities())
+            {
+                var query = from item in context.Alphabetical_list_of_products
+                            select new
+                            {
+                                a = item.CategoryName,
+                                b = item.ProductName,
+                                c = item.QuantityPerUnit
+                            };
+
+                foreach (var item in query )
+                {
+                    Console.WriteLine(
+                        "a={0},b={1},c={2}",item.a,item.b,item.c);
+                }
+            }
         }
     }
 }
