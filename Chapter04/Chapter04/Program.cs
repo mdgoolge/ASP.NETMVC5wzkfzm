71b7931ec2bc012db3ea0b5a9a79b9424c3a787d
mdgoogle <gengtiwin@hotmail.com>
2018-08-01 11:16:00 +0800

    a

--- Chapter04/Chapter04/Program.cs
+++ Chapter04/Chapter04/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,21 +13,34 @@ namespace Chapter04
         static void Main(string[] args)
         {
 
-            using (northwndEntities context=new northwndEntities())
-            {
-                var query = from item in context.Alphabetical_list_of_products
-                            select new
-                            {
-                                a = item.CategoryName,
-                                b = item.ProductName,
-                                c = item.QuantityPerUnit
-                            };
+            //using (northwndEntities context=new northwndEntities())
+            //{
+            //    var query = from item in context.Alphabetical_list_of_products
+            //                select new
+            //                {
+            //                    a = item.CategoryName,
+            //                    b = item.ProductName,
+            //                    c = item.QuantityPerUnit
+            //                };
 
-                foreach (var item in query )
-                {
-                    Console.WriteLine(
-                        "a={0},b={1},c={2}",item.a,item.b,item.c);
-                }
+            //    foreach (var item in query )
+            //    {
+            //        Console.WriteLine(
+            //            "a={0},b={1},c={2}",item.a,item.b,item.c);
+            //    }
+            //}
+
+            decimal totalDue = 500.00M;
+            using (AdventureWorks2012Entities  context = new AdventureWorks2012Entities())
+            {
+                DbSet<ContactType> contacts = context.ContactType;
+                DbSet<SalesOrderHeader> orders = context.SalesOrderHeader;
+
+                //var query=
+                //    from  contact in contacts
+                //    from  order in orders
+                //    where contact.ContactTypeID==orde
+
             }
         }
     }
