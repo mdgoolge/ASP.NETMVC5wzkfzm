e6c63ef5855ae2f3f45c8685a6b80eaf7dd45621
mdgoogle <gengtiwin@hotmail.com>
2018-08-01 23:10:52 +0800

    a

--- Chapter04/CodeFirst/BlogDbModel.cs
+++ Chapter04/CodeFirst/BlogDbModel.cs
@@ -20,12 +20,12 @@
         //为您要在模型中包含的每种实体类型都添加 DbSet。有关配置和使用 Code First  模型
         //的详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=390109。
 
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<MyEntity> MyEntities { get; set; }
     }
 
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
+    public class MyEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
 }
