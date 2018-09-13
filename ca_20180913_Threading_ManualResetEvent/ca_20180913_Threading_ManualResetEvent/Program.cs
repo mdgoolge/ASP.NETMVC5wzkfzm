9564bb2c5ac10051d8f0b3215348c33d0a2e3758
mdgoogle <gengtiwin@hotmail.com>
2018-09-13 15:01:47 +0800

    a

---
+++ ca_20180913_Threading_ManualResetEvent/ca_20180913_Threading_ManualResetEvent/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ca_20180913_Threading_ManualResetEvent
+{
+    class Program
+    {
+        //一开始设置为false才会等待收到信号才执行
+        static ManualResetEvent mr = new ManualResetEvent(false);
+        static void Main(string[] args)
+        {
+        }
+    }
+}
